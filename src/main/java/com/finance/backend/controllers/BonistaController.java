a799108bd6fc73e8b7f0fa63e6566af71aa84242
Frany-oss <francesco8bassino@gmail.com>
2022-06-17 11:25:34 -0500

    update bonista controller

--- src/main/java/com/finance/backend/controllers/BonistaController.java
+++ src/main/java/com/finance/backend/controllers/BonistaController.java
@@ -3,6 +3,7 @@ package com.finance.backend.controllers;
 import com.finance.backend.models.BonistaDto;
 import com.finance.backend.models.CreateBonistaDto;
 import com.finance.backend.models.LoginBonistaDto;
+import com.finance.backend.models.UpdateBonistaDto;
 import com.finance.backend.services.BonistaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,22 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/bonista")
 @RequiredArgsConstructor
 public class BonistaController {
-  private final BonistaService bonistaService;
+	private final BonistaService bonistaService;
 
-  @PostMapping("/signup")
-  public BonistaDto registerBonista(@RequestBody CreateBonistaDto createBonistaDto)
-      throws Exception {
-    return bonistaService.registerBonista(createBonistaDto);
-  }
+	@PostMapping("/signup")
+	public BonistaDto registerBonista(@RequestBody CreateBonistaDto createBonistaDto)
+			throws Exception {
+		return bonistaService.registerBonista(createBonistaDto);
+	}
 
-  @PostMapping("/login")
-  public BonistaDto loginBonista(@RequestBody LoginBonistaDto login) throws Exception {
-    return bonistaService.loginBonista(login);
-  }
+	@PostMapping("/login")
+	public BonistaDto loginBonista(@RequestBody LoginBonistaDto login) throws Exception {
+		return bonistaService.loginBonista(login);
+	}
 
-  @PutMapping("/update")
-  public BonistaDto updateBonista(@RequestBody CreateBonistaDto createBonistaDto)
-      throws Exception {
-    return bonistaService.updateBonista(createBonistaDto);
-  }
+	@PutMapping("/update")
+	public BonistaDto updateBonista(@RequestBody UpdateBonistaDto updateBonistaDto) throws Exception {
+		return bonistaService.updateBonista(updateBonistaDto);
+	}
 }
