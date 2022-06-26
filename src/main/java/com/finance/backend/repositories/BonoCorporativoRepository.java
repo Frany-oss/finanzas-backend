82b5ee3541eb1f36b364a0bc3fdb145233566001
Brayan Rodriguez-Rojas <u201924448@upc.edu.pe>
2022-06-26 00:07:12 -0500

    Update BonoCorporativoRepository.java

--- src/main/java/com/finance/backend/repositories/BonoCorporativoRepository.java
+++ src/main/java/com/finance/backend/repositories/BonoCorporativoRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BonoCorporativoRepository extends JpaRepository<BonoCorporativo, Long> {
+	
   List<BonoCorporativo> findAll();
 
   Optional<BonoCorporativo> findById(Long id);
